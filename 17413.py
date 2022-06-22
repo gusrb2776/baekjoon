d3761278608ee05947b87d241cc4a2140e762530
gusrb2776 <gusrb2776@naver.com>
2022-06-22 16:55:06 +0900

    17413

--- 17413.py
+++ 17413.py
@@ -3,19 +3,29 @@ import sys
 
 input = sys.stdin.readline
 
-s = input().split()
-index = 0
+s = input().rstrip()
 
-for sentence in s:
-    stack = deque()
-    for i in sentence:
+word = ''
+tag = False
+result = ''
+
+for i in s:
+    if tag == False:
         if i == '<':
-            index = 1
-        elif i == '>':
-            index = 0
-        if index == 0 and i != '>':
-            stack.appendleft(i)
+            tag = True
+            word = word + i
+        elif i == ' ':  #공백이면 단어를 구분지음 --> 공백일때 result에 넣어주기
+            word = word + i
+            result = result + word
+            word = ''
+        else:
+            word = i + word     #뒤집어서 출력위해
 
-    for j in stack:
-        print(j,end='')
-    print(' ',end='')
+    else:
+        word = word + i
+        if i == '>':
+            tag = False
+            result = result + word
+            word = ''
+
+print(result+word)
